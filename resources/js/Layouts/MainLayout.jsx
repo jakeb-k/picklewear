02a30e46d9a9f5cb1f37886a451affc585c7bf51
jakeb-k <jknow30@outlook.com>
2024-08-17 14:43:08 +1000

    [FEAT] setup login functionality

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -1,55 +1,34 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import { Link, usePage } from "@inertiajs/react";
+import axios from "axios";
 
-export default function MainLayout({ auth, children }) {
+export default function MainLayout({ children }) {
+    const { auth } = usePage().props;
+
     return (
         <div className="flex flex-col min-h-screen sm:pt-0 bg-gray-100 relative">
-             <nav className="w-full bg-gray-800 text-white">
-                    {auth?.user ? (
-                        <Link
-                            href={route("dashboard")}
-                        >
-                            Dashboard
-                        </Link>
-                    ) : (
-                        <div className='w-1/3 flex flex-row justify-center space-x-8'>
-                            <Link
-                                href={route("login")}
-                            >
-                                Log in
-                            </Link>
-                            <Link
-                                href={route("register")}
-                            >
-                                Register
-                            </Link>
-                        </div>
-                    )}
-                </nav>
-            <div className='flex flex-col flex-grow'>
-                {children}
-            </div>
+            <nav className="w-full bg-gray-800 text-white">
+                {auth?.user ? (
+                    <Link href={route("logout")} method="post" as="button">
+                        Log Out
+                    </Link>
+                ) : (
+                    <div className="w-1/3 flex flex-row justify-center space-x-8">
+                        <Link href={route("login")}>Log in</Link>
+                        <Link href={route("register")}>Register</Link>
+                    </div>
+                )}
+            </nav>
+            <div className="flex flex-col flex-grow">{children}</div>
             <footer className="w-full bg-gray-800 text-white absolute bottom-0">
-            {auth?.user ? (
-                        <Link
-                            href={route("dashboard")}
-                        >
-                            Dashboard
-                        </Link>
-                    ) : (
-                        <div className='w-1/3 flex flex-row justify-center space-x-8'>
-                            <Link
-                                href={route("login")}
-                            >
-                                Log in
-                            </Link>
-                            <Link
-                                href={route("register")}
-                            >
-                                Register
-                            </Link>
-                        </div>
-                    )}
+                {auth?.user ? (
+                    <Link href={route("logout")} method="post" as="button">Logout</Link>
+                ) : (
+                    <div className="w-1/3 flex flex-row justify-center space-x-8">
+                        <Link href={route("login")}>Log in</Link>
+                        <Link href={route("register")}>Register</Link>
+                    </div>
+                )}
             </footer>
         </div>
     );
