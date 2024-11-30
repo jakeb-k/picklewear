435c8cc084134b0083aab13c9bccb560f9d9c3a6
jakeb-k <jknow30@outlook.com>
2024-11-30 14:42:31 +1000

    [UI] added success ui for contact btn

--- resources/js/Components/ContactForm.jsx
+++ resources/js/Components/ContactForm.jsx
@@ -113,9 +113,13 @@ export default function ContactForm() {
                     className={`rounded-lg py-1 px-4 bg-transparent min-h-32 hover:bg-gray-200/50 focus:ring-2 focus:ring-[#FFD100] focus:outline-none transition-all duration-150 ease-in-out ${error?.message ? 'border-red-500' : ''}`}
                 />
             </div>
-            <div className="flex w-full justify-end">
+            <div className="flex w-full justify-end items-center space-x-8">
+                <p>
+                    {success}
+                </p>
                 <button onClick={() => sendEmail()}
-                className={`hover:bg-main w-1/4 text-secondary hover:text-secondary border-2 border-secondary transition-all duration-200 ease-in-out italic text-3xl font-bold px-4 py-2 rounded-lg flex items-center justify-center`}>
+                disabled={success}
+                className={`hover:bg-main w-1/4 text-secondary hover:text-secondary border-2 border-secondary transition-all duration-200 ease-in-out italic text-3xl font-bold px-4 py-2 rounded-lg flex items-center justify-center ${success ?'bg-green-500 text-white hover:bg-green-500 hover:text-white' :''}`}>
                     {loading && (<svg
                         className="animate-spin -ml-1 mr-3 h-5 w-5 text-secondary"
                         xmlns="http://www.w3.org/2000/svg"
@@ -136,6 +140,7 @@ export default function ContactForm() {
                             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                         ></path>
                     </svg>)}
+                    {success && (<i className="fa-regular fa-circle-check text-white mr-2"></i>)}
                     SEND
                 </button>
             </div>
