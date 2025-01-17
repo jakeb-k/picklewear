bac2479023433bdc2b1c119438b8ee9b6b0fad25
jakeb-k <jknow30@outlook.com>
2025-01-18 09:59:25 +1000

    [TEST] unit test for location model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'first_name' => fake()->name(),
             'last_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email' => $this->faker->phoneNumber(),
+            'mobile' => $this->faker->phoneNumber(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
--- tests/Unit/LocationTest.php
+++ tests/Unit/LocationTest.php
@@ -2,13 +2,54 @@
 
 namespace Tests\Unit;
 
+use App\Models\Customer;
+use App\Models\Location;
+use App\Models\Order;
+use App\Models\User;
 use Tests\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
 class LocationTest extends TestCase
 {
+    public Location $location; 
+
+    public Order $order; 
+
+    public Customer $customer;
+
+    public User $user; 
+
     protected function setUp(): void
     {
         parent::setUp(); // Always call the parent setUp method
+        $this->location = Location::factory()->create();
+
+        $this->order = Order::factory()->create();
+        $this->order->locations()->attach($this->location);
+
+        $this->customer = Customer::factory()->create();
+        $this->customer->locations()->attach($this->location);
+
+        
+        $this->user = User::factory()->create();
+        $this->user->locations()->attach($this->location);
+    }
+
+    #[Test]
+    public function a_location_can_be_morphed_by_an_order():void
+    {
+        $this->assertEquals($this->order->id, $this->location->order()->first()->id);
+    }
+
+    #[Test]
+    public function a_location_can_be_morphed_by_a_customer():void
+    {
+        $this->assertEquals($this->customer->id, $this->location->customer()->first()->id);
+    }
+
+    #[Test]
+    public function a_location_can_be_morphed_by_a_user():void
+    {
+        $this->assertEquals($this->user->id, $this->location->user()->first()->id);
     }
 }
