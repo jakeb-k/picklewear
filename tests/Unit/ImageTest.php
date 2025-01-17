c25f4bef4936370bcff27e7c83f1a9c352be1feb
jakeb-k <jknow30@outlook.com>
2025-01-18 08:41:32 +1000

    [FEAT] generate intial unit test files

---
+++ tests/Unit/ImageTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ImageTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
