da2d4639d38b80d5e756b89893e0a9691ef7077a
jakeb-k <jknow30@outlook.com>
2025-01-18 10:18:31 +1000

    [TEST] product option unit test

--- tests/Unit/ProductOptionTest.php
+++ tests/Unit/ProductOptionTest.php
@@ -2,13 +2,29 @@
 
 namespace Tests\Unit;
 
+use App\Models\Product;
+use App\Models\ProductOption;
 use Tests\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
 class ProductOptionTest extends TestCase
 {
+    public ProductOption $productOption;
+    
+    public Product $product;
+
     protected function setUp(): void
     {
         parent::setUp(); // Always call the parent setUp method
+        $this->product = Product::factory()->create();
+        $this->productOption = ProductOption::factory()->create([
+            'product_id' => $this->product->id, 
+        ]);
+    }
+
+    #[Test]
+    public function a_product_option_belongs_to_a_product():void
+    {
+        $this->assertEquals($this->productOption->id, $this->product->options()->first()->id);
     }
 }
