739527b8eac7012d7dd1e75081fcee42313cff7a
jakeb-k <jknow30@outlook.com>
2025-04-05 17:51:53 +1000

    [FIX] ui issue with search results

--- resources/js/Components/home/SearchResults.jsx
+++ resources/js/Components/home/SearchResults.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import TestImage from "@/../assets/images/testing_imgs/test_1.webp"; 
+import TestImage from "@/../assets/images/testing_imgs/test_1.webp";
 
 export default function SearchResults({ query }) {
     const [message, setMessage] = useState("...loading");
@@ -35,20 +35,37 @@ export default function SearchResults({ query }) {
         }, 2500);
     }, [products]);
 
+    console.log(products);
     return (
-        <div id='show-scroll' className="lg:w-3/4 mx-auto h-[75vh] bg-white rounded-t-none rounded-md px-10 py-4 overflow-y-scroll">
+        <div
+            id="show-scroll"
+            className="lg:w-3/4 mx-auto h-[75vh] bg-white rounded-t-none rounded-md px-10 py-4 overflow-y-scroll"
+        >
             {products.length > 0 && !loading ? (
-               <div className='flex flex-row flex-wrap justify-start w-full '>
-               {products.map((product) => (
-                   <a href={route('products.show', product.id)} className="flex flex-col relative w-[30%] mr-[2.5%] mb-4 p-4 min-h-[250px] text-center rounded-md bg-white hover:bg-gray-200/25 group z-20 cursor-pointer overflow-hidden transition-all duration-300"> {/* Adjusted width and added bottom margin */}
-                   <div className="absolute inset-0 bg-gray-200/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20"></div>
-                       <img className='w-full' src={product.images[0]?.file_path ?? TestImage } />
-                       <p className='font-bold z-30'>{product.name}</p>
-                       <p className='mt-auto z-30 font-roboto_mono'>${product.price}</p>
-                   </a>
-               ))}
-                                
-           </div>
+                <div className="flex flex-row flex-wrap justify-start w-full ">
+                    {products.map((product) => (
+                        <a
+                            href={route("products.show", product.id)}
+                            className="flex flex-col relative w-[30%] mr-[2.5%] mb-4 p-4 min-h-[250px] text-center rounded-md bg-white hover:bg-gray-200/25 group z-20 cursor-pointer overflow-hidden transition-all duration-300"
+                        >
+                            {" "}
+                            {/* Adjusted width and added bottom margin */}
+                            <div className="absolute inset-0 bg-gray-200/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-20"></div>
+                            <img
+                                className="w-full"
+                                src={product.images[0]?.file_path ?? TestImage}
+                            />
+                            <div className='mt-auto'>
+                                <p className="font-bold z-30">
+                                    {product.name}
+                                </p>
+                                <p className="z-30 font-roboto_mono">
+                                    ${product.price.toFixed(2)}
+                                </p>
+                            </div>
+                        </a>
+                    ))}
+                </div>
             ) : (
                 <p>{message}</p>
             )}
